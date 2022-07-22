99614fa49e83d9dd81ef9f16de30eab7583d4555
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-22 00:08:28 -0700

    Initial Commit

---
+++ CreateGraphics-SwiftUI-Stocks/CreateGraphics_SwiftUI_StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateGraphics_SwiftUI_StocksApp.swift
+//  CreateGraphics-SwiftUI-Stocks
+//
+//  Created by Mitya Kim on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct CreateGraphics_SwiftUI_StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
